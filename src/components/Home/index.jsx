f83012cd2642c60ad1d3e0243b0163f5afbec3ad
Leigh Forrest <leighmforrest@gmail.com>
2019-09-25 19:28:44 -0400

    database code added

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React from "react";
 
+import { withAuthorization } from "../Session";
 
 const HomePage = () => (
-    <div>HomePage</div>
-)
- 
-export default HomePage;
+  <div>
+    <h1>HomePage</h1>
+    <p>The Home Page is accessible by every signed in user.</p>
+  </div>
+);
+
+const condition = (authUser) => !!authUser;
+
+export default withAuthorization(condition)(HomePage);
